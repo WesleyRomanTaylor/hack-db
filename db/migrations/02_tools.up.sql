9025fc1001711d285bb50fa7d3ce5fea51ca8ebe
Wing-Sea Poon <wsp93@users.noreply.github.com>
2021-05-21 15:36:20 -0700

    Fix migrations (#12)
    * Fix migrations
    
    * Generate UUID by default
    
    * Add default UUID to other tables
    
    * Update trigger

--- db/migrations/02_tools.up.sql
+++ db/migrations/02_tools.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tools (
-        id UUID primary key,
+        id UUID primary key DEFAULT uuid_generate_v1(),
         title citext NOT NULL UNIQUE,
         tool_code text NOT NULL,
         created_by text NOT NULL,
@@ -7,10 +7,10 @@ CREATE TABLE tools (
         vote_count integer NOT NULL DEFAULT 1,
         broken_count integer NOT NULL DEFAULT 0,
         created_at timestamptz NOT NULL DEFAULT current_timestamp,
-        updated_at timestamptz NOT NULL DEFAULT current_timestamp,
+        updated_at timestamptz NOT NULL DEFAULT current_timestamp
 );
 
 CREATE TRIGGER tools_update
-        BEFORE UPDATE OF tags, votes ON tools
+        BEFORE UPDATE OF title, tool_code, description, vote_count, broken_count ON tools
         FOR EACH ROW
         EXECUTE PROCEDURE update_timestamp();
